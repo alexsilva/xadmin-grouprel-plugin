5be466c08a292a3161e5b38f56714c41a03bf926
alexsilva <geniofuturo@gmail.com>
2018-10-18 13:33:35 -0300

    init files.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='xadmin-grouprel-plugin',
+    version='1.0',
+    packages=['xplugin_grouprel'],
+    url='https://github.com/alexsilva/xadmin-grouprel-plugin',
+    license='MIT',
+    author='alex',
+    author_email='alex@fabricadigital.com.br',
+    description='xadmin plugin that displays user related to a group (datatable)'
+)
