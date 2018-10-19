3f0f46931f3b71396990de4fa27ea62b01fc5606
alexsilva <geniofuturo@gmail.com>
2018-10-19 12:58:58 -0300

    Adding translation pt-br.

--- xplugin_grouprel/static/xplugin-grouprel/js/group.table.handler.js
+++ xplugin_grouprel/static/xplugin-grouprel/js/group.table.handler.js
@@ -1,5 +1,6 @@
 (function ($) {
     var csrftoken = $.getCookie('csrftoken');
+    var static_url = window.__admin_media_prefix__.replace(/xadmin\/$/i, "xplugin-grouprel/");
     $('#ajax-table').DataTable({
         "ajax": {
             "url": ajax_table_url,
@@ -7,6 +8,9 @@
             beforeSend: function (xhr, settings) {
                 xhr.setRequestHeader("X-CSRFToken", csrftoken);
             }
+        },
+        "language": {
+            "url": static_url + window.__admin_language_code__ + ".json"
         },
         "processing": true,
         "serverSide": true,
