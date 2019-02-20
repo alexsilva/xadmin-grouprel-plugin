6bca705ecaa1a4106427a44106db5710edf952c7
alexsilva <geniofuturo@gmail.com>
2019-02-20 20:25:21 -0300

    clean format.

--- xplugin_grouprel/static/xplugin-grouprel/js/group.table.handler.js
+++ xplugin_grouprel/static/xplugin-grouprel/js/group.table.handler.js
@@ -3,20 +3,20 @@
     var static_url = window.__admin_media_prefix__.replace(/xadmin\/$/i, "xplugin-grouprel/");
     var table = $('#ajax-table').DataTable({
         dom: 'Blfrtip',
-        "ajax": {
-            "url": grouprel_datatable.ajax.url,
-            "type": "POST",
+        ajax: {
+            url: grouprel_datatable.ajax.url,
+            type: "POST",
             beforeSend: function (xhr, settings) {
                 xhr.setRequestHeader("X-CSRFToken", csrftoken);
             }
         },
-        "language": {
-            "url": static_url + window.__admin_language_code__ + ".json"
+        language: {
+            url: static_url + window.__admin_language_code__ + ".json"
         },
-        "scrollX": true,
-        "processing": true,
-        "serverSide": true,
-        "columnDefs": grouprel_datatable.columns_defs,
+        scrollX: true,
+        processing: true,
+        serverSide: true,
+        columnDefs: grouprel_datatable.columns_defs,
         buttons: {
             buttons: [{
                 text: grouprel_datatable.button.text,
