451033b9c346fe8191abfc3c611a8e472f738e46
alexsilva <geniofuturo@gmail.com>
2019-02-26 13:03:04 -0300

    Setup modal lg.

--- xplugin_grouprel/static/xplugin-grouprel/js/grouprel.plugin.quick-form.js
+++ xplugin_grouprel/static/xplugin-grouprel/js/grouprel.plugin.quick-form.js
@@ -168,7 +168,7 @@
             var self = this;
 
             if (!this.modal) {
-                this.modal = $('<div class="modal fade quick-form-modal" role="dialog"><div class="modal-dialog"><div class="modal-content">' +
+                this.modal = $('<div class="modal fade quick-form-modal" role="dialog"><div class="modal-dialog modal-lg"><div class="modal-content">' +
                     '<div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button><h3>' +
                     this.$btn.attr('title') + '</h3></div><div class="modal-body"></div>' +
                     '<div class="modal-footer" style="display: none;"><button class="btn btn-default" data-dismiss="modal" aria-hidden="true">' + gettext('Close') + '</button>' +
