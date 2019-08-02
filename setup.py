8ff016f9b510a0721339961b54604e675380b9e8
alexsilva <geniofuturo@gmail.com>
2019-08-02 18:56:34 -0300

    Add templatetags to install.

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 setup(
     name='xadmin-grouprel-plugin',
     version='2.0',
-    packages=['xplugin_grouprel'],
+    packages=['xplugin_grouprel',
+              'xplugin_grouprel.templatetags'],
     install_requires=["django-datatables-view"],
     url='https://github.com/alexsilva/xadmin-grouprel-plugin',
     license='MIT',
