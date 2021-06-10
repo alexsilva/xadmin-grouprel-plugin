8f64b728a71bae4cc6d255e066744964abb44cc7
alexsilva <geniofuturo@gmail.com>
2021-06-10 17:15:48 -0300

    Remove fade efect.

--- xplugin_grouprel/static/xplugin-grouprel/js/grouprel.plugin.quick-form.js
+++ xplugin_grouprel/static/xplugin-grouprel/js/grouprel.plugin.quick-form.js
@@ -168,7 +168,7 @@
             var self = this;
 
             if (!this.modal) {
-                this.modal = $('<div class="modal fade quick-form-modal" role="dialog"><div class="modal-dialog modal-lg"><div class="modal-content">' +
+                this.modal = $('<div class="modal quick-form-modal" role="dialog"><div class="modal-dialog modal-lg"><div class="modal-content">' +
                     '<div class="modal-header"><h5 class="modal-title">' + this.$btn.attr('title') + '</h5>' +
                     '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button></div><div class="modal-body"></div>' +
                     '<div class="modal-footer" style="display: none;"><button class="btn btn-secondary" data-dismiss="modal" aria-hidden="true">' + gettext('Close') + '</button>' +
