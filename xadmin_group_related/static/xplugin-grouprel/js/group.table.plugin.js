ae4b7ac9a50cd728c15d45b7dbdbdf0e25698be3
alexsilva <geniofuturo@gmail.com>
2024-04-05 19:44:42 -0300

    multiple declaration of variables.

--- xadmin_group_related/static/xplugin-grouprel/js/group.table.plugin.js
+++ xadmin_group_related/static/xplugin-grouprel/js/group.table.plugin.js
@@ -1,41 +1,41 @@
 (function ($) {
     $.fn.create_table = function (datatable_config) {
-        var tokenInput = $('[name=csrfmiddlewaretoken]');
-        var csrftoken = tokenInput.val() || $.getCookie('csrftoken');
-        var static_url = window.xadmin.media_prefix.replace(/xadmin\/$/i, "xplugin-grouprel/");
-        var config = {
-            ajax: {
-                url: datatable_config.ajax.url,
-                type: "POST",
-                beforeSend: function (xhr, settings) {
-                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
-                }
-            },
-            language: {
-                url: static_url + window.xadmin.language_code.toLowerCase() + ".json",
+        var tokenInput = $('[name=csrfmiddlewaretoken]'),
+            csrftoken = tokenInput.val() || $.getCookie('csrftoken'),
+            static_url = window.xadmin.media_prefix.replace(/xadmin\/$/i, "xplugin-grouprel/"),
+            config = {
+                ajax: {
+                    url: datatable_config.ajax.url,
+                    type: "POST",
+                    beforeSend: function (xhr, settings) {
+                        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                    }
+                },
+                language: {
+                    url: static_url + window.xadmin.language_code.toLowerCase() + ".json",
+                    select: {
+                    rows: {
+                        _: gettext("%d rows selected"),
+                        0: gettext("Click a row to select it"),
+                        1: gettext("1 row selected")
+                    }
+                    }
+                },
+                order: datatable_config.get_columns_order(),
+                scrollX: true,
+                processing: true,
+                serverSide: true,
                 select: {
-                rows: {
-                    _: gettext("%d rows selected"),
-                    0: gettext("Click a row to select it"),
-                    1: gettext("1 row selected")
-                }
-                }
-            },
-            order: datatable_config.get_columns_order(),
-            scrollX: true,
-            processing: true,
-            serverSide: true,
-            select: {
-                style: 'multi',
-                selector: 'td:first-child'
-            },
-            columnDefs: datatable_config.columns_defs,
-            initComplete: function () {
-                if (datatable_config.hasOwnProperty("initComplete")) {
-                    datatable_config.initComplete()
-                }
-            },
-        };
+                    style: 'multi',
+                    selector: 'td:first-child'
+                },
+                columnDefs: datatable_config.columns_defs,
+                initComplete: function () {
+                    if (datatable_config.hasOwnProperty("initComplete")) {
+                        datatable_config.initComplete()
+                    }
+                },
+            };
         if (datatable_config.hasOwnProperty("buttons")) {
             config.dom = 'Blfrtip';
             config.buttons = datatable_config.buttons
